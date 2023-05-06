3bfc1d7ce65c94e736a7469de104cf71e45175df
Rob Cerda <16689532+robcerda@users.noreply.github.com>
2023-05-05 19:50:46 -0700

    revamp

--- geolocation.js
+++ geolocation.js
@@ -1,26 +1,20 @@
-const delayInMinutes = 1;
-const intervalInMinutes = 1;
-
-chrome.alarms.create('fetchLocation', {
-  delayInMinutes,
-  periodInMinutes: intervalInMinutes,
-});
-
-chrome.alarms.onAlarm.addListener(async (alarm) => {
-  if (alarm.name === 'fetchLocation') {
+function getLocation() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  }
+  
+  async function fetchLocation() {
     try {
-      const position = await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-      });
-
+      const position = await getLocation();
       const { latitude, longitude } = position.coords;
       const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&zoom=18&addressdetails=1`);
       const locationData = await response.json();
-
+  
       const city = locationData.address.city || locationData.address.town || locationData.address.village;
       const state = locationData.address.state;
       const country = locationData.address.country;
-
+  
       const locationInfo = {
         latitude,
         longitude,
@@ -28,18 +22,20 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
         state,
         country,
       };
-
+  
       const blob = new Blob([JSON.stringify(locationInfo, null, 2)], { type: 'application/json' });
       const url = URL.createObjectURL(blob);
-
+  
       chrome.downloads.download({
         url: url,
         filename: `geolocation_${Date.now()}.json`,
         conflictAction: 'uniquify',
       });
-
+  
     } catch (error) {
       console.error('Failed to fetch location:', error);
     }
   }
-});
+  
+  fetchLocation();
+  
--- popup.js
+++ popup.js
@@ -1,26 +1,17 @@
-const delayInMinutes = 1;
-const intervalInMinutes = 1;
-
-chrome.alarms.create('fetchLocation', {
-  delayInMinutes,
-  periodInMinutes: intervalInMinutes,
-});
-
-chrome.alarms.onAlarm.addListener(async (alarm) => {
-  if (alarm.name === 'fetchLocation') {
+document.getElementById('getLocation').addEventListener('click', async () => {
     try {
       const position = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject);
       });
-
+  
       const { latitude, longitude } = position.coords;
       const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&zoom=18&addressdetails=1`);
       const locationData = await response.json();
-
+  
       const city = locationData.address.city || locationData.address.town || locationData.address.village;
       const state = locationData.address.state;
       const country = locationData.address.country;
-
+  
       const locationInfo = {
         latitude,
         longitude,
@@ -28,18 +19,18 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
         state,
         country,
       };
-
+  
       const blob = new Blob([JSON.stringify(locationInfo, null, 2)], { type: 'application/json' });
       const url = URL.createObjectURL(blob);
-
+  
       chrome.downloads.download({
         url: url,
         filename: `geolocation_${Date.now()}.json`,
         conflictAction: 'uniquify',
       });
-
+  
     } catch (error) {
       console.error('Failed to fetch location:', error);
     }
-  }
-});
+  });
+  
